f45b0b42b3fe5aaca18dd8fa2de4ee1fd59e429b
Arianrod <tatyana.comolyh@gmail.com>
2024-02-28 22:49:23 +0300

    Развёрнуто окружение для cypress

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+describe('My First Test', () => {
+	it('Does not do much!', () => {
+		expect(true).to.equal(true);
+	});
+});
