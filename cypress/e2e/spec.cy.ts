aa5661d1028930216435e8f9257a588b302248ab
Arianrod <tatyana.comolyh@gmail.com>
2024-04-02 00:31:39 +0300

    Удаление cypress

--- cypress/e2e/spec.cy.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import { describe, it } from 'mocha';
-
-describe('My First Test', () => {
-	it('Does not do much!', () => {
-		expect(true).to.equal(true);
-	});
-});
